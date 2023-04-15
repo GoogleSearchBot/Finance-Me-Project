92aa5b19e57729caa5930e1a29408d6ca990092a
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-15 21:31:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ resource "aws_security_group" "mysg" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
