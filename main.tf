ecf3c2658d9b20d637b3a9e5a85dbbf612577eea
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-15 14:48:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,38 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAWYYXEWHS37OKFYU2"
-  secret_key = "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"
+  access_key = "AKIAWYYXEWHSQTMNMZHE"
+  secret_key = "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"
+}
+
+
+resource "aws_security_group" "mysg" {
+  name_prefix = "finance-me-sg"
+  vpc_id      = "vpc-037232b62dfa10afc"
+  description = "Example security group"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "example-security-group"
+  }
 }
 
 resource "aws_instance" "FinanceMeDeploy" {
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
-  key_name      = "Kushal"
-  vpc_security_group_ids = ["sg-0906b8b9eb805dc82"]
+  vpc_security_group_ids = [aws_security_group.mysg.id]
   tags = {
     Name = "FinanceMEDeploy"
   }
@@ -20,3 +44,8 @@ resource "null_resource" "ansible_provisioner" {
     command = "ansible-playbook -i '${aws_instance.FinanceMeDeploy.public_ip},' Ubuntu-config.yml"
   }
 }
+
+
+
+
+
