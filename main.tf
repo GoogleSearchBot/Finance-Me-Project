16dc6db590524283d1b373aff65f09e3956d8499
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-15 14:29:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "FinanceMeDeploy" {
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
   key_name      = "Kushal.pem"
-  security_groups = ["sg-0906b8b9eb805dc82"]
+  vpc_security_group_ids = ["sg-0906b8b9eb805dc82"]
   tags = {
     Name = "FinanceMEDeploy"
   }
